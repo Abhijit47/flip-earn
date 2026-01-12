5f35963e58bc731a9df8f2922f2925b6148b9c2d
Abhijit47 <akarmakar846@gmail.com>
2026-01-12 20:52:44 +0530

    03:Added Authentication and login and signup page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import ThemeToggle from '@/components/shared/theme-toggle';
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -7,6 +8,8 @@ export default function Home() {
       <h1 className={'text-4xl font-bold'}>Buy & Sell your Social</h1>
       <Button>Click here</Button>
       <ThemeToggle />
+      <Link href={'/login'}>Login</Link>
+      <Link href={'/sign-up'}>Sign up</Link>
     </>
   );
 }
