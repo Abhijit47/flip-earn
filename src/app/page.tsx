9d4e6928e6db26af43726a883d39fe1c7625d350
Abhijit47 <akarmakar846@gmail.com>
2026-01-12 21:39:37 +0530

    04:Added TRPC with basic setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,28 @@
+import { ClientGreeting } from '@/components/client-greeting';
 import ThemeToggle from '@/components/shared/theme-toggle';
 import { Button } from '@/components/ui/button';
+import { caller, HydrateClient, prefetch, trpc } from '@/trpc/server';
 import Link from 'next/link';
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 
-export default function Home() {
+export default async function Home() {
+  const greeting = await caller.hello({ text: 'from tRPC server' });
+  prefetch(trpc.hello.queryOptions({ text: 'from tRPC server' }));
+
   return (
-    <>
+    <HydrateClient>
+      {greeting.greeting}
       <h1 className={'text-4xl font-bold'}>Buy & Sell your Social</h1>
       <Button>Click here</Button>
       <ThemeToggle />
       <Link href={'/login'}>Login</Link>
       <Link href={'/sign-up'}>Sign up</Link>
-    </>
+      <ErrorBoundary fallback={<div>Something went wrong</div>}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <ClientGreeting />
+        </Suspense>
+      </ErrorBoundary>
+    </HydrateClient>
   );
 }
