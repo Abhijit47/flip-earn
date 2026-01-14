d5b27c975639fab576f75e01460aa8379797b993
Abhijit47 <akarmakar846@gmail.com>
2026-01-14 17:53:58 +0530

    08:Added navbar and footer

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,7 +1,16 @@
+import Footer from '@/components/shared/footer';
+import Navbar from '@/components/shared/navbar';
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <main>{children}</main>;
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
 }
